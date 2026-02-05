162dc68be0cc97e29d78c41a25456fbc9bb235ef
Juset Reynoso <jreynoso280988@gmail.com>
2026-02-05 11:54:26 -0400

    Agregando http import

--- core/views.py
+++ core/views.py
@@ -641,6 +641,7 @@ from django.http import HttpResponse
 def test_email_view(request):
     try:
         import os
+        from django.http import HttpResponse
         from django.core.mail import send_mail
         from django.conf import settings
         
