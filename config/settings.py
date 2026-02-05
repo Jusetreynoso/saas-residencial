d8670f203942f3847468c6ce110bd1ad5881ae21
Juset Reynoso <jreynoso280988@gmail.com>
2026-02-05 12:19:05 -0400

    Cambiando a puerto 465 SSL para evitar timeout

--- config/settings.py
+++ config/settings.py
@@ -160,8 +160,9 @@ LOGOUT_REDIRECT_URL = '/accounts/login/' # Al salir, ir al login
 # --- CONFIGURACIÓN DE CORREO (DINÁMICA) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 # Leemos las variables de la nube. Si no existen, usa las de respaldo (tu PC).
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'saasresidencial@gmail.com')
