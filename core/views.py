c9689ea9203b41f49d4855b7371e6998c2ed575b
Juset Reynoso <jreynoso280988@gmail.com>
2026-03-12 17:31:23 -0400

    Fix: Importacion correcta de update_session_auth_hash

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages 
 from django.core.exceptions import ValidationError
 import json 
